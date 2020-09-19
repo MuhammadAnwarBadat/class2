3e0e974e69a346696a9ace89bb12b6d7c7d97546
anwar_badat <anwarbadat1997.ab@gmail.com>
2020-09-19 23:33:45 +0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <App name="Anwar Badat" age={25} isReactDeveloper={true} />
+  </div>,  
+
   document.getElementById('root')
 );
 
